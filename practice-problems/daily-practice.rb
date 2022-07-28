0790602a34ad4c91dea7f3fd7e0e622543656a26
fellowbeard <virgovibe@gmail.com>
2022-07-28 10:13:13 -1000

    daily practice

--- practice-problems/daily-practice.rb
+++ practice-problems/daily-practice.rb
@@ -56,3 +56,4 @@
 
 # 10. Start with an array of numbers and create a new array with each number converted into a string.
 #     For example, [1, 2, 3] becomes ["1", "2", "3"].
+
--- practice-problems/trello.rb
+++ practice-problems/trello.rb
@@ -707,31 +707,32 @@
 # {title: 'Mondays are the worst', submitted_by: "Aunty Em", likes: 644}
 # ]
 
-def roman_to_int(s)
-  roman_nums = {
-      "I" => 1,
-      "V" => 5,
-      "X" => 10,
-      "L" => 50,
-      "C" => 100,
-      "D" => 500,
-      "M" => 1000
-  }
-  string = s.split("")
-  num_values = []
-  string.each do |char|
-      num_values << roman_nums[char]
-  end
-  total = 0
-  num_values.each_with_index do |num, index|
-    if num_values[index + 1] && num >= num_values[index + 1]
-        total += num
-    else
-        total -= num 
-    end
-  end
-  total
-end
-p roman_to_int("MCMXCIV")
+# def roman_to_int(s)
+#   roman_nums = {
+#       "I" => 1,
+#       "V" => 5,
+#       "X" => 10,
+#       "L" => 50,
+#       "C" => 100,
+#       "D" => 500,
+#       "M" => 1000
+#   }
+#   string = s.split("")
+#   num_values = []
+#   string.each do |char|
+#       num_values << roman_nums[char]
+#   end
+#   total = 0
+#   num_values.each_with_index do |num, index|
+#     if num_values[index + 1] && num >= num_values[index + 1]
+#         total += num
+#     else
+#         total -= num 
+#     end
+#   end
+#   total
+# end
+# p roman_to_int("MCMXCIV")
+
 
 
