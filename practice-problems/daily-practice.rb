b08657d797b7b7ba253c069c4ae7168f8b30cbd4
fellowbeard <virgovibe@gmail.com>
2022-08-12 11:17:49 -1000

    daily practice

--- practice-problems/daily-practice.rb
+++ practice-problems/daily-practice.rb
@@ -81,20 +81,33 @@
 #  2. Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
 #     For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
 
-pairs = [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}]
-pairs_hash = {}
-index = 0
+# pairs = [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}]
+# pairs_hash = {}
+# index = 0
 
-while index < pairs.length
-  pairs_hash[pairs[index][:id]] = pairs[index]
-  index += 1
-end
-p pairs_hash
+# while index < pairs.length
+#   pairs_hash[pairs[index][:id]] = pairs[index]
+#   index += 1
+# end
+# p pairs_hash
 
 
 
 #  3. Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
 #     For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
+
+word = "bookkeeper"
+letter_frequencies = {}
+index = 0
+while index < word.length
+  letter = word[index]
+  if letter_frequencies[letter] == nil
+    letter_frequencies[letter] = 0
+  end
+  letter_frequencies[letter] += 1
+  index += 1
+end
+p letter_frequencies
 
 #  4. Convert a hash into an array of arrays.
 #     For example, {"chair" => 100, "book" => 14} becomes [["chair", 100], ["book", 14]].
