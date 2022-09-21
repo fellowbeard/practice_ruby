2cca94dbf2fbf23686b7de9aab92d7d9908f392d
fellowbeard <virgovibe@gmail.com>
2022-09-20 15:37:11 -1000

    daily practice

--- practice-problems/daily-practice.js
+++ practice-problems/daily-practice.js
@@ -195,6 +195,13 @@
 
 // 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
 
+while (true) {
+  var input = window.prompt("Enter a number: ");
+  if (parseInt(input) > 10) {
+    break;
+  }
+}
+
 // 7. Write a while loop that prints the numbers 50 to 70.
 
 // 8. Write a while loop that prints the phrase "Around the world" 144 times.
