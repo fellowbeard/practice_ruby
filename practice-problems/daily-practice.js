a1a1c987516f4fc7d4f269b3843005ff72ac8f35
fellowbeard <virgovibe@gmail.com>
2022-09-20 15:25:56 -1000

    daily practice

--- practice-problems/daily-practice.js
+++ practice-problems/daily-practice.js
@@ -149,6 +149,32 @@
 
 // 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
 
-var countries = ["brasil", "mexico", "argentina"];
-countries.push("peru");
-console.log(countries);
+// var countries = ["brasil", "mexico", "argentina"];
+// countries.push("peru");
+// console.log(countries);
+
+// 1. Write a while loop to print the numbers 1 through 10.
+
+// var num = 1;
+// while (num <= 10) {
+//   console.log(num);
+//   num += 1;
+// }
+
+// 2. Write a while loop that prints the word "hello" 5 times.
+
+// 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
+
+// 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+// 5. Write a while loop that prints the number 9000 ten times.
+
+// 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
+
+// 7. Write a while loop that prints the numbers 50 to 70.
+
+// 8. Write a while loop that prints the phrase "Around the world" 144 times.
+
+// 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
+
+// 10. Write a while loop that prints the even numbers from 2 to 40.
