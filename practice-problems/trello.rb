ba9ff4a2ba688aa16e196a911c0e096a890216bd
fellowbeard <virgovibe@gmail.com>
2022-09-21 14:18:26 -1000

    daily practice

--- practice-problems/trello.rb
+++ practice-problems/trello.rb
@@ -798,3 +798,20 @@
 # end
 
 # p reduce([1, 2, 3, 4])
+
+# Given an array, write a function that returns an array that contains the original array’s values in reverse.
+
+# Input: [1, 2, 3, 4, 5]
+# Output: [5, 4, 3, 2, 1]
+
+# def reverse (array)
+#   new_array = []
+#   i = array.length - 1
+#   while i >= 0
+#     new_array << array[i]
+#     i -= 1
+#   end
+#   return new_array
+# end
+
+# p reverse([1, 2, 3, 4, 5])
