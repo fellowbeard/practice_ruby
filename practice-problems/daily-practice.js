eeef587eb3e9f2a68f311a1d299d26e54eae90ea
fellowbeard <virgovibe@gmail.com>
2022-10-19 14:59:50 -0500

    daily practice

--- practice-problems/daily-practice.js
+++ practice-problems/daily-practice.js
@@ -262,13 +262,37 @@
 
 // 10. Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
 
-var num = 71;
-if (num > 80) {
-  console.log(5);
-} else if (num > 60) {
-  console.log(4);
-} else if (num > 40) {
-  console.log(3);
-} else {
-  console.log(2);
-}
+// var num = 71;
+// if (num > 80) {
+//   console.log(5);
+// } else if (num > 60) {
+//   console.log(4);
+// } else if (num > 40) {
+//   console.log(3);
+// } else {
+//   console.log(2);
+// }
+
+// 1. Write a program that uses variables to store a first and last name, then prints the full name in one line using string concatenation (the + operator).
+
+var firstName = "Joe";
+var lastName = "Shmo";
+console.log(firstName + " " + lastName);
+
+// 2. Write a program that uses variables to store a first and last name, then prints the full name in one line using string interpolation (the //{} operator).
+
+// 3. Write a program that asks the user to input a word. If the word is "marco", print "polo".
+
+// 4. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string concatenation (the + operator).
+
+// 5. Write a program that uses variables to store three different colors, then prints out a sentence using the colors with string interpolation (the //{} operator).
+
+// 6. Write a program that asks the user to enter a name. If the name is not "Santa", print "You're not Santa."
+
+// 7. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string concatenation (the + operator).
+
+// 8. Write a program that uses variables to store a book's title and author, then prints out a sentence using that information with string interpolation (the //{} operator).
+
+// 9. Write a program that asks the user to enter a password. If the password is "Joshua", the program responds "Shall we play a game?". For any other password, the program responds "Access denied"
+
+// 10. Write a program that uses variables to store the names of three cities, then prints out a sentence using that information with string concatenation (the + operator).
