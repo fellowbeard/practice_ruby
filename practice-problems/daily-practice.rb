36d72d61b31945237ac7b621ea817c9b4f4dd547
fellowbeard <virgovibe@gmail.com>
2023-03-05 13:49:30 -0600

    daily

--- practice-problems/daily-practice.rb
+++ practice-problems/daily-practice.rb
@@ -44,16 +44,22 @@
 
 # 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
 
-while true
-  puts "Enter a number: "
-  num = gets.chomp.to_i
-  if num > 10
-    break
-  end
-end
+# while true
+#   puts "Enter a number: "
+#   num = gets.chomp.to_i
+#   if num > 10
+#     break
+#   end
+# end
     
 
 # 7. Write a while loop that prints the numbers 50 to 70.
+
+count = 50
+while count <= 70
+  p count
+  count += 1
+end
 
 # 8. Write a while loop that prints the phrase "Around the world" 144 times.
 
