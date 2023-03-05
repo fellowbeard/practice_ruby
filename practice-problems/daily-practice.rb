bab3047284fb77e4c8e55efa107ab9c1ca483852
fellowbeard <virgovibe@gmail.com>
2023-03-05 09:56:54 -0600

    daily

--- practice-problems/daily-practice.rb
+++ practice-problems/daily-practice.rb
@@ -6,13 +6,17 @@
 
 # 2. Write a program that asks the user to enter a number, then prints "That's a big number" if the number is greater than 100.
 
-puts "Enter a number: "
-number = gets.chomp
-if number.to_i > 100
-  puts "Thats a big number!"
-end
+# puts "Enter a number: "
+# number = gets.chomp
+# if number.to_i > 100
+#   puts "Thats a big number!"
+# end
 
 # 3. Write a program that asks the user to enter two numbers, then prints the numbers added together.
+puts "Enter two numbers: "
+num1 = gets.chomp
+num2 = gets.chomp
+p num1.to_i + num2.to_i
 
 # 4. Write a program that asks the user to enter a word, then prints that word in reverse order.
 
