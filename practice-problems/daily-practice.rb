20458b9bd9192d66acfaa987f7fcef4b7bb05b35
fellowbeard <virgovibe@gmail.com>
2023-03-05 13:50:19 -0600

    daily

--- practice-problems/daily-practice.rb
+++ practice-problems/daily-practice.rb
@@ -55,13 +55,19 @@
 
 # 7. Write a while loop that prints the numbers 50 to 70.
 
-count = 50
-while count <= 70
-  p count
-  count += 1
-end
+# count = 50
+# while count <= 70
+#   p count
+#   count += 1
+# end
 
 # 8. Write a while loop that prints the phrase "Around the world" 144 times.
+
+count = 0
+while count <= 144
+  p "Around the world"
+  count += 1
+end
 
 # 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
 
