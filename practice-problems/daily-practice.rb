192511b5a5ee6c2715e0a1d230e0b13b36bda5b0
fellowbeard <virgovibe@gmail.com>
2023-03-05 15:20:22 -0600

    daily

--- practice-problems/daily-practice.rb
+++ practice-problems/daily-practice.rb
@@ -24,13 +24,17 @@
 
 # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
 
-array = [1]
-array << 2
-array << 3
-array << 4
-p array
+# array = [1]
+# array << 2
+# array << 3
+# array << 4
+# p array
 
 # 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+
+array = ["one", "two", "three"]
+array[2] = array[2].upcase
+p array
 
 # 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
 
