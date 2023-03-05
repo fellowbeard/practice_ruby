50b6861730e00fc3dbc360cc8fde34de6228b01e
fellowbeard <virgovibe@gmail.com>
2023-03-05 08:43:15 -0600

    daily practice

--- practice-problems/daily-practice.rb
+++ practice-problems/daily-practice.rb
@@ -1,15 +1,25 @@
 # 1. Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
 
-a = 10
+# a = 10
 
-if a == 10
-  p 0
-else
-  p -1
-end
+# if a == 10
+#   p 0
+# else
+#   p -1
+# end
 
 
 # 2. Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+a = 95
+
+if a < 10
+  p -1
+elsif a > 10
+  p 1
+else a === 10
+  p 0
+end
 
 # 3. Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
 
