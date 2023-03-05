356770e49fc9d5767e4a8602c174cbd649a997bb
fellowbeard <virgovibe@gmail.com>
2023-03-05 15:15:35 -0600

    daily

--- practice-problems/daily-practice.rb
+++ practice-problems/daily-practice.rb
@@ -1,13 +1,17 @@
 # 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
 
-array = ["one", "two", "three"]
-array << "four"
-array << "five"
+# array = ["one", "two", "three"]
+# array << "four"
+# array << "five"
 
-p array
+# p array
 
 
 # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+
+array = ["a", "b", "c", "d"]
+array[1] = "e"
+p array
 
 # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
 
