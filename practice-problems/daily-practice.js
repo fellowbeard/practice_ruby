558465ab2bc9373abe6f6293df8e42b1da171547
fellowbeard <virgovibe@gmail.com>
2023-03-09 09:02:03 -0600

    daily

--- practice-problems/daily-practice.js
+++ practice-problems/daily-practice.js
@@ -50,12 +50,37 @@
 
 // # 8. Write a while loop that prints the phrase "Around the world" 144 times.
 
-let count = 0;
-while (count <= 144) {
-  console.log("Around the world");
-  count += 1;
-}
+// let count = 0;
+// while (count <= 144) {
+//   console.log("Around the world");
+//   count += 1;
+// }
 
 // # 9. Write a while loop that asks the user to enter a word and will run forever until the user enters a word with more than 5 letters.
 
 // # 10. Write a while loop that prints the even numbers from 2 to 40.
+
+// # 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+let array = ["guitar", "piano", "drums"];
+array.push("cowbell");
+array.push("flute");
+console.log(array);
+
+// # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+
+// # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+// # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+
+// # 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+
+// # 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+
+// # 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+
+// # 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+
+// # 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+
+// # 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
