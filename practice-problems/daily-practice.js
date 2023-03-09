3c54604d5d47ec816bfb61574853ceacd3c6798e
fellowbeard <virgovibe@gmail.com>
2023-03-09 08:57:32 -0600

    daily

--- practice-problems/daily-practice.js
+++ practice-problems/daily-practice.js
@@ -32,15 +32,21 @@
 
 // # 5. Write a while loop that prints the number 9000 ten times.
 
-let count = 0;
-while (count <= 10) {
-  console.log(9000);
-  count += 1;
-}
+// let count = 0;
+// while (count <= 10) {
+//   console.log(9000);
+//   count += 1;
+// }
 
 // # 6. Write a while loop that asks the user to enter a number and will run forever until the user enters a number greater than 10.
 
 // # 7. Write a while loop that prints the numbers 50 to 70.
+
+let count = 50;
+while (count <= 70) {
+  console.log(count);
+  count += 1;
+}
 
 // # 8. Write a while loop that prints the phrase "Around the world" 144 times.
 
