557a4cab9b185de67beaf5e3cce65a2ee2a67c86
fellowbeard <virgovibe@gmail.com>
2023-03-09 09:05:09 -0600

    daily

--- practice-problems/daily-practice.js
+++ practice-problems/daily-practice.js
@@ -69,11 +69,18 @@
 
 // # 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
 
-let letters = ["a", "b", "c", "d"];
-letters[1] = 34;
-console.log(letters);
+// let letters = ["a", "b", "c", "d"];
+// letters[1] = 34;
+// console.log(letters);
 
 // # 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+let numbers = [1, 2, 3, 4, 5];
+let index = 0;
+while (index < numbers.length) {
+  console.log(numbers[index]);
+  index += 1;
+}
 
 // # 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
 
