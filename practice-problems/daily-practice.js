e8e3010481d8ece8b749468fb6c71e46ae13603e
fellowbeard <virgovibe@gmail.com>
2023-03-09 08:54:36 -0600

    daily

--- practice-problems/daily-practice.js
+++ practice-problems/daily-practice.js
@@ -15,7 +15,20 @@
 
 // # 3. Write a while loop that asks the user to enter a word and will run forever until the user enters the word "stop".
 
+// while (true) {
+//   let input = window.prompt("Enter a word: ");
+//   if (input === "stop") {
+//     break;
+//   }
+// }
+
 // # 4. Write a while loop that prints the numbers 0 through 100, increasing by 5 each time.
+
+let num = 0;
+while (num <= 100) {
+  console.log(num);
+  num += 5;
+}
 
 // # 5. Write a while loop that prints the number 9000 ten times.
 
